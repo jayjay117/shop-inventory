a5511162f415978ebf5d5e4a44193476dc1ad60d
Joseph Nehikhare <nehisjoseph@gmail.com>
2024-12-03 21:23:53 +0100

    added logout logic

--- js/dashboard.js
+++ js/dashboard.js
@@ -21,7 +21,7 @@ document.addEventListener('DOMContentLoaded', function () {
     const logoutButton = document.getElementById('logout-button');
     logoutButton.addEventListener('click', function () {
         localStorage.clear();
-        window.location.href = 'login.html';
+        window.location.href = 'index.html';
     });
 
    
